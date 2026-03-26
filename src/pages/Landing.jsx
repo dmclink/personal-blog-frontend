cc168bc90109c77c527eeb2d77cd7e4c72f128fa
dmclink <135415819+dmclink@users.noreply.github.com>
2026-03-26 19:40:40 +0800

    feat: add text to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,18 @@
-import { usePostSummariesContext } from '../contexts/PostSummariesContext.jsx';
 import { Link } from 'react-router-dom';
 
-function Landing(props) {
-	const context = usePostSummariesContext();
-	const isLoading = context.isLoading;
-	const posts = context.posts;
-	const postsError = context.error;
-
+function Landing() {
 	return (
-		<section>
-			<h1>hello laanding page</h1>
+		<section className="landing-section">
+			<p>This is a personal blog site made and written by dmclink.</p>
+			<p>
+				This was built as a practice and demo of React and Node.js. And at the time I'm writing this landing
+				page, I'm not sure if I will actually maintain it or not. If there are any posts, topics will include
+				computer programming. Discussing the struggles I find as I reinvent many wheels.
+			</p>
+			<p>
+				Currently getting a shallow depth of knowledge of a number of JavaScript frameworks and libraries. I
+				miss coding in Go, hopefully I can get back to it soon.
+			</p>
 			<p>
 				<Link to="/home">Go to home</Link>
 			</p>
